62f267677706b31ede6a8dc64eb2a8474f9c3360
AliSadeghian2007 <amiras8416@gmail.com>
2026-02-07 01:02:32 +0330

    adding part (counting prime number)

--- Prime Divisor.py
+++ Prime Divisor.py
@@ -3,8 +3,20 @@ def is_prime(num):
         return False
     if num==2:
         return True
+    k=True
     for i in range (2,num):
         if num %i==0:
-            return False
-        else:
-            return True
+            k=False
+    if k==False:
+        return False
+    else:
+        return True
+
+def count_prime_Divisor(n):
+    count=0
+    for i in range (1,n+1):
+        if n%i==0:
+            x=is_prime(i)
+            if x==True:
+                count+=1
+        return count
